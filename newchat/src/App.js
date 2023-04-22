9874619ac6f564b155fc5282772814c4e0e6d610
SanKocho_de_Pollo <84156615+NoName588@users.noreply.github.com>
2023-04-21 22:41:01 -0500

    sus

---
+++ newchat/src/App.js
@@ -0,0 +1,19 @@
+import { ChatEngine } from "react-chat-engine";
+
+import './App.css';
+
+const App = () => {
+
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="e621b78c-2146-4188-9b34-7ba7a7d0c240"
+            userName="JavaMaster"
+            userSecret="1234"
+        
+        />
+    );
+    
+}
+
+export default App;
